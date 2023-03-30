a43947ca854f816ba8f8f621f6b20b628014bf87
carmengomezmena <62596618+carmengomezmena@users.noreply.github.com>
2023-03-30 12:32:36 +0200

    plot graphs

--- PlotL2Graphs.py
+++ PlotL2Graphs.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+#### THIS FILE IS OUR OF DATE - WE ARE USING LOCAL VERSION ####
+
 # Set the path to your CSV file
 loss_path = 'csv_files/loss_curve.csv'
 
@@ -11,5 +14,18 @@ data_loss = pd.read_csv(loss_path)
 print(data_loss)
 
 
+plt.figure()
 plt.plot(data_loss['Iteration'],data_loss['L1_loss'], '.')
+plt.title('L1_Loss Curve')
+plt.xlabel('Iteration')
+plt.ylabel('L1_loss')
+plt.grid()
+plt.show()
+
+plt.figure()
+plt.plot(data_loss['Iteration'],data_loss['MS_SSIM'], '.')
+plt.title('MS_SSIM Curve')
+plt.xlabel('Iteration')
+plt.ylabel('MS_SSIM')
+plt.grid()
 plt.show()
