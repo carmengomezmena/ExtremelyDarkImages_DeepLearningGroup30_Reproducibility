18dcc6c03a0ad8508b9f97e719a46ef7c7c08344
sirbugsalot <nadav.eldar@hotmail.com>
2023-03-30 14:53:05 +0200

    dff

--- Restoring-Extremely-Dark-Images-In-Real-Time-main/NIQE_check.m
+++ Restoring-Extremely-Dark-Images-In-Real-Time-main/NIQE_check.m
@@ -2,7 +2,6 @@
 % Cycles through the images in local folder demo_restored_images
 % For each image, it will run the NIQUE (and get a Ma value too?)
 
-
 %% NIQE
 
 path        = 'demo_restored_images/';
@@ -12,12 +11,20 @@ if (isempty(files))
 else
     niqe_array  = zeros(length(files));
     niqe_array  = niqe_array(1,:);
+    brisque_arr = zeros(length(files));
+    brisque_arr = brisque_arr(1,:);
     
     i           = 1;
     for file = files'
         address         = [path file.name];
         I               = imread(address);
         niqe_array(i)   = niqe(I);
+        brisque_arr(i)   = brisque(I);
         i               = i + 1;
     end
 end
+disp('niqe scores:');
+disp(niqe_array);
+disp(' ');
+disp('brisque scores:');
+disp(brisque_arr);
